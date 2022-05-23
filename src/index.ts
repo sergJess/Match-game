916a79ba3ec569a4e85126d6f6193e62b36f7851
sergJess <serg.fantasy@yandex.ru>
2022-05-23 21:04:24 +0300

    feat: add styles for start page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import './base-styles.css';
 import {NodeCreator, nodeParametrs} from './helpers/node-creator';
+import StartPage from './start-page/start-page';
 
 const root = new NodeCreator(document.body, document.createElement('div'),{id: 'root'}).getElement();
+
+new StartPage(root, document.createElement('div'),{}).render();
