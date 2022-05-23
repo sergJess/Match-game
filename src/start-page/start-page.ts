236294119d591664583c27e0cafc39843385977c
sergJess <serg.fantasy@yandex.ru>
2022-05-23 15:40:12 +0300

    feat: add render method

--- src/start-page/start-page.ts
+++ src/start-page/start-page.ts
@@ -1,8 +1,14 @@
 import './start-page.css';
 import {NodeCreator, nodeParametrs} from '../helpers/node-creator';
 
-class StartPage extends NodeCreator {
+export default class StartPage extends NodeCreator {
   constructor(parent: HTMLElement, element: HTMLElement, nodeParams: nodeParametrs){
     super(parent, element, nodeParams);
+  }
+  render(): void {
+    const startPageInner = new NodeCreator(this.getElement(), document.createElement('div'),{classList: ['start-page-inner']});
+    const startButton = new NodeCreator(startPageInner.getElement(), document.createElement('button'),{classList: ['start-button'],
+     text: 'Start Game'
+  });
   }
 }
