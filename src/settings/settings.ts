ce7f3ef8014dc5f616bfe7516e0b147f17e066c1
sergJess <serg.fantasy@yandex.ru>
2022-05-27 22:39:15 +0300

    feat: add styles for settings

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -9,24 +9,14 @@ export default class Settings extends NodeCreator{
 		super(parent, element, nodeParams);
 }
 
-// initiateByButton(parentNode: HTMLElement, callback: voidFunc): void{
-// const buttonStart = new NodeCreator(parentNode, document.createElement('button'), {classList: ['settings-start-button'],
-// text: 'Settings'
-// }).getElement();
-// buttonStart.onclick = (): void => {
-// 	callback();
-// };
-
-// }
-
 render(): void{
 	const title = new NodeCreator(this.getElement(), document.createElement('h2'), {classList: ['settings-title'],
 text: 'Settings'
 });
 const settingsInner = new NodeCreator(this.getElement(), document.createElement('div'), {classList:['settings-inner']});
-const firstPoint = new NodeCreator(settingsInner.getElement(), document.createElement('div'), {text: 'Choose difficulty'});
-const secondPoint = new NodeCreator(settingsInner.getElement(), document.createElement('div'), {text: 'Set volume range'});
-const thirdPoint = new NodeCreator(settingsInner.getElement(), document.createElement('div'), {text: 'Choose song'});
+const firstPoint = new NodeCreator(settingsInner.getElement(), document.createElement('div'), {classList:['settings-point-inner'], text: 'Choose difficulty'});
+const secondPoint = new NodeCreator(settingsInner.getElement(), document.createElement('div'), {classList:['settings-point-inner'], text: 'Set volume range'});
+const thirdPoint = new NodeCreator(settingsInner.getElement(), document.createElement('div'), {classList:['settings-point-inner'], text: 'Choose song'});
 
 }
 
