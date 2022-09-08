ace8ca7f1e7602f922e274778a48a881369ad656
sergJess <serg.fantasy@yandex.ru>
2022-09-08 09:25:35 +0300

    feat: add styles for info button on start page

--- src/info-game/info.ts
+++ src/info-game/info.ts
@@ -12,10 +12,9 @@ export default class InfoGame extends NodeCreator{
 					const infoTitle = new NodeCreator(document.createElement('h2'), {classList: ['info-title'], text: 'About Game'}, this.getElement());
 					const infoScreen = new NodeCreator(document.createElement('div'),{classList: ['info-screen'], text: 'In Match Game Player must remember where lie the same opened cards. When the timer is over, all cards reverse. Then Player must open the cards one by one, the score increases only when opened only two the same.'}, this.getElement());
 			const backToStartPage = new NodeCreator(document.createElement('button'),{classList: ['info-start-page'], text: 'Back to Start Page'}, this.getElement());
-		backToStartPage.setOnclick = (():void => {
+		backToStartPage.setOnclick(():void => {
 	   new Eraser([this]).erase();
     new StartPage(document.createElement('div'),{classList: ['start-page']}, content).render();
-				console.log('Jess')
 		});
 
 		
