9affca1957af14826d7a7ba7bc1dd1d2944941ee
sergJess <serg.fantasy@yandex.ru>
2022-09-08 09:20:57 +0300

    feat: add info page

---
+++ src/info-game/info.ts
@@ -0,0 +1,23 @@
+import './info.css';
+import {NodeCreator, INodeParametrs} from '../helpers/node-creator';
+import Eraser from '../helpers/node-eraser';
+import {content} from '../index';
+import StartPage from '../start-page/start-page';
+
+export default class InfoGame extends NodeCreator{
+		constructor( element: HTMLElement, nodeParams: INodeParametrs, parent?: HTMLElement){
+		super(element, nodeParams, parent);
+  }
+		render(): void {
+					const infoTitle = new NodeCreator(document.createElement('h2'), {classList: ['info-title'], text: 'About Game'}, this.getElement());
+					const infoScreen = new NodeCreator(document.createElement('div'),{classList: ['info-screen'], text: 'In Match Game Player must remember where lie the same opened cards. When the timer is over, all cards reverse. Then Player must open the cards one by one, the score increases only when opened only two the same.'}, this.getElement());
+			const backToStartPage = new NodeCreator(document.createElement('button'),{classList: ['info-start-page'], text: 'Back to Start Page'}, this.getElement());
+		backToStartPage.setOnclick = (():void => {
+	   new Eraser([this]).erase();
+    new StartPage(document.createElement('div'),{classList: ['start-page']}, content).render();
+				console.log('Jess')
+		});
+
+		
+		}
+}
--- src/start-page/start-page.ts
+++ src/start-page/start-page.ts
@@ -3,6 +3,7 @@ import {NodeCreator, INodeParametrs} from '../helpers/node-creator';
 import NodeEraser from '../helpers/node-eraser';
 import Game from '../game/game';
 import Settings from '../settings/settings';
+import InfoGame from '../info-game/info';
 
 export default class StartPage extends NodeCreator {
   constructor( element: HTMLElement, nodeParams: INodeParametrs, parent?: HTMLElement){
@@ -21,6 +22,12 @@ export default class StartPage extends NodeCreator {
 startSettings.setOnclick(():void =>{
   new NodeEraser([this]).erase();
   new Settings(document.createElement('div'),{classList: ['settings-page']}, {difficulty: 'medium'}, this.getParent()).render();
+});
+
+const info = new NodeCreator(document.createElement('button'),{classList: ['start-page-info'], text: 'Info'}, startPageInner.getElement());
+info.setOnclick(()=>{
+  new NodeEraser([this]).erase();
+  new InfoGame(document.createElement('div'), {classList: ['info-page']}, this.getParent()).render();
 });
 
 startButton.setOnclick(():void =>{
